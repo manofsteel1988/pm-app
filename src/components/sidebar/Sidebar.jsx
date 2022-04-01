a47652e51400c27eb9a4454c73e98b620e9350c1
Cyrille Bomba <c.bombangong@fulll.fr>
2022-04-01 16:28:18 +0200

    Avatar in sidebar done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./Sidebar.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import Avatar from "../avatar/Avatar";
+
 import DashboardIcon from "../../assets/dashboard_icon.svg";
 import AddIcon from "../../assets/add_icon.svg";
 
+import "./Sidebar.css";
+
 const Sidebar = () => {
+  const { user } = useAuthContext();
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          <p>Hey user</p>
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
